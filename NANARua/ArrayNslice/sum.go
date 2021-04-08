94e62455fa55e33a2aab4ceb8a487be8c4f0d88e
NANARua <nanaruahu@gmail.com>
2021-04-08 18:04:02 +0800

    2

--- NANARua/ArrayNslice/sum.go
+++ NANARua/ArrayNslice/sum.go
@@ -1,9 +1,21 @@
 package ArrayNslice
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+
+	// for i := 0; i < 5; i++ {
+	// 	sum += numbers[i]
+	// }
+
+	//运用range写法更简洁
+	//range 会迭代数组，每次迭代都会返回数组元素的索引和值。我们选择使用 _ 空白标志符 来忽略索引。
+	// for _, number := range numbers {
+	// 	sum += number
+	// }
+
+	//数组长度属于类型的一部分，切片类型尺寸不固定，比较灵活
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
--- NANARua/ArrayNslice/sum_test.go
+++ NANARua/ArrayNslice/sum_test.go
@@ -3,12 +3,22 @@ package ArrayNslice
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collections of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+	t.Run("collections of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
