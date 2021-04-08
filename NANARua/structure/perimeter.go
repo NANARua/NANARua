fc805c9650f9ba9a45eee588b1c0b5e63790fb61
NANARua <nanaruahu@gmail.com>
2021-04-08 19:26:47 +0800

    2

--- NANARua/structure/perimeter.go
+++ NANARua/structure/perimeter.go
@@ -4,6 +4,6 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
