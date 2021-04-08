c11462cb305157e8e064d2c90db82991279a90be
NANARua <nanaruahu@gmail.com>
2021-04-08 15:53:00 +0800

    mult test

--- NANARua/hello/Hello_test.go
+++ NANARua/hello/Hello_test.go
@@ -3,11 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello world"
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
-	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("yiqi")
+		want := "Hello yiqi"
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello world!"
+
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
 }
 
 // run "go test in termial" to text the function
