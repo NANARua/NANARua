1b97bf4613ff1fcc7adc70934b9ba794a094de08
NANARua <nanaruahu@gmail.com>
2021-04-08 15:39:42 +0800

    test

--- NANARua/hello/Hello_test.go
+++ NANARua/hello/Hello_test.go
@@ -4,8 +4,10 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	got := Hello()
-	want := "Hello world!"
+	want := "Hello world"
 	if got != want {
 		t.Errorf("got '%q' want '%q'", got, want)
 	}
 }
+
+// run "go test in termial" to text the function
