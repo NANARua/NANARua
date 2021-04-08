76c165f376b8dfe6fc62cb9071dc7187bd0e4a83
NANARua <nanaruahu@gmail.com>
2021-04-08 19:00:51 +0800

    1

---
+++ NANARua/structure/perimeter_test.go
@@ -0,0 +1,11 @@
+package structure
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+	if got != want {
+		t.Errorf("got %2.f want %w.f", got, want)
+	}
+}
