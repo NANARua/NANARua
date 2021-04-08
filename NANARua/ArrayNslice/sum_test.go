0763991975ceb393f42afe55adef774bab387ae0
NANARua <nanaruahu@gmail.com>
2021-04-08 18:36:03 +0800

    final sum

--- NANARua/ArrayNslice/sum_test.go
+++ NANARua/ArrayNslice/sum_test.go
@@ -1,6 +1,9 @@
 package ArrayNslice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -13,12 +16,20 @@ func TestSum(t *testing.T) {
 		}
 	})
 
-	t.Run("collections of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-		got := Sum(numbers)
-		want := 6
-		if want != got {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
+	// t.Run("collections of any size", func(t *testing.T) {
+	// 	numbers := []int{1, 2, 3}
+	// 	got := Sum(numbers)
+	// 	want := 6
+	// 	if want != got {
+	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	// 	}
+	// })
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{3, 4, 5})
+	want := 15
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
